8bbfb7a727831f0110546da40727b37c8e02a756
jubairJnu <jubair2810@gmail.com>
2023-05-20 22:51:55 +0600

    added-job-img

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <main className='container flex main'>
         <div className="text">
           <h1>One Step <br></br>Closer To Your <br />
-            <span> Dream Job</span>
+            <span className='purple'> Dream Job</span>
           </h1>
           <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
           <button className='btn-linier'>Get Started</button>
--- src/Component/JobCatagory/JobCatagory.jsx
+++ src/Component/JobCatagory/JobCatagory.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import './JobCatagory.css'
 const JobCatagory = ({work}) => {
-  const {job, available} = work;
+  const {job, available,img} = work;
   return (
-    <div>
+    <div className='one-job'>
+      <img src={img} alt="" />
       <h5>{job}</h5>
       <p>{available} <span> Jobs Available</span></p>
+      
     </div>
   );
 };
